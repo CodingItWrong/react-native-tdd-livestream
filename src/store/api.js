70b4ef8ce6334ae3002c6365c5600bf2ce0d6353
Josh Justice <me@codingitwrong.com>
2018-09-14 14:55:00 -0400

    Read API config from .env file

--- src/store/api.js
+++ src/store/api.js
@@ -1,11 +1,12 @@
+import env from 'react-native-config';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://sandboxapi.codingitwrong.com/',
+  baseURL: env.API_URL,
   headers: {
     // TODO: do not hard code this
     'Authorization':
-    'Bearer 779931e4b49cda548a2cb849854840a8c33403703c399a8bcb1c53d6cd91a27c',
+    `Bearer ${env.AUTH_TOKEN}`,
   },
 });
 
